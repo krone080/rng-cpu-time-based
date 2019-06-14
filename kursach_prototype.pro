20baa0afe0f268d4801590e4e7d37e1b06348140
Ivan Pavlov (u2) <stone08@mail.ru>
2019-06-14 14:07:28 +0300

    C13: механизм с частотой процессора

--- kursach_prototype.pro
+++ kursach_prototype.pro
@@ -5,6 +5,13 @@ CONFIG -= qt
 
 SOURCES += main.c
 
+LIBS += -L$$PWD/../../../../usr/lib/gcc/x86_64-linux-gnu/7/ -lasan -lubsan
+INCLUDEPATH += $$PWD/../../../../usr/lib/gcc/x86_64-linux-gnu/7/include/sanitizer
+
+QMAKE_CFLAGS += -Wall -Wextra -pedantic -O2 -Wshadow -Wformat=2 -Wfloat-equal
+QMAKE_CFLAGS += -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual
+QMAKE_CFLAGS += -Wcast-align -D_GLIBC_DEBUG -D_GLIBC_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2
+QMAKE_CFLAGS += -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fstack-protector
 
 #INCLUDING LIBPAPI ON MY GENTOO SYSTEM
 #win32:CONFIG(release, debug|release): LIBS += -L$$PWD/../../../../usr/local/lib64/release/ -lpapi
@@ -21,9 +28,9 @@ SOURCES += main.c
 #else:unix: PRE_TARGETDEPS += $$PWD/../../../../usr/local/lib64/libpapi.a
 
 #INCLUDING LIBPAPI ON MY UBUNTU SYSTEM
-unix:!macx: LIBS += -L$$PWD/../../../../usr/local/lib/ -lpapi
+#unix:!macx: LIBS += -L$$PWD/../../../../usr/local/lib/ -lpapi
 
-INCLUDEPATH += $$PWD/../../../../usr/local/include
-DEPENDPATH += $$PWD/../../../../usr/local/include
+#INCLUDEPATH += $$PWD/../../../../usr/local/include
+#DEPENDPATH += $$PWD/../../../../usr/local/include
 
-unix:!macx: PRE_TARGETDEPS += $$PWD/../../../../usr/local/lib/libpapi.a
+#unix:!macx: PRE_TARGETDEPS += $$PWD/../../../../usr/local/lib/libpapi.a
--- main.c
+++ main.c
@@ -8,24 +8,24 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <fcntl.h>
-#include <papi.h>
 #include <unistd.h>
 #include <math.h>
+#include <string.h>
 
 //Получение параметров нормального распределения в текущий момент
 //времени
-double get_rand(const unsigned lcnt);
+double get_rand(const unsigned lcnt, unsigned *freq);
 
-double get_rand_devi(const double meanX, const unsigned lcnt)
- {return get_rand(lcnt)-meanX;}
+double get_rand_devi(const double meanX, const unsigned lcnt,unsigned *freq)
+ {return get_rand(lcnt,freq)-meanX;}
 
 double get_rand_devi_sum(const double meanX, const unsigned lcnt, const unsigned n);
 
-int distribution_init(double *meanX, double *meanS, double *varS, const unsigned cnt,
+void distribution_init(double *meanX, double *meanS, double *varS, const unsigned cnt,
                       const unsigned lcnt, const unsigned n);
 
-int distribution_init_test(double *meanX, double *meanS, double *varS, const unsigned cnt,
-                      const unsigned lcnt, const unsigned n);
+void distribution_init_test(double *meanX, double *meanS, double *varS, const unsigned cnt,
+                      const unsigned lcnt, const unsigned n, char *fname);
 
 double normal_cdf(double x, double E, double D)
  {return 0.5*(1.+erf((x-E)/sqrt(2.*D)));}
@@ -46,13 +46,13 @@ int main(int argc,char* argv[])
 //2. fork, создание особых потомков для генерации различных
 //   независимых выборок
 
- pid_t pid;
- struct timespec comt1,comt2;
- int fd;
+// pid_t pid;
+// struct timespec comt1,comt2;
  char fname[20];
  unsigned cnt,n,lcnt,cnt2;
- float rtime,ptime,ipc;
- long_long ins;
+ FILE *ffile;
+// float rtime,ptime,ipc;
+// long long ins;
 
  if(argc==6)
   {
@@ -75,20 +75,21 @@ int main(int argc,char* argv[])
   printf("Gen sample length: ");
   scanf("%i",&cnt2);
   }
- close(1);
- fd=creat(fname,0664);
- if(fd<0)
-  {
-  perror("creat()");
-  return -1;
-  }
+// fd=creat(fname,0664);
+// if(fd<0)
+//  {
+//  perror("creat()");
+//  return -1;
+//  }
 
+
+// ffile=fopen(fname,"w");
 // Параметры цикла (счётчик) будут, скорее всего, изменяемыми и будут
 // зависеть от оценок, посчитанных заранее
 
  double varS;
  double meanX,meanS;
- distribution_init_test(&meanX,&meanS,&varS,cnt,lcnt,n);
+ distribution_init_test(&meanX,&meanS,&varS,cnt,lcnt,n,fname);
 
 // printf("\nES=%lli, DS=%e",meanS,varS);
 // for(unsigned i=0;i<cnt2;++i)
@@ -112,35 +113,43 @@ int main(int argc,char* argv[])
  *
  *
  */
-double get_rand(const unsigned lcnt)
+double get_rand(const unsigned lcnt, unsigned *freq)
  {
  void* stat=NULL;
  struct rusage info;
- struct timespec t1,t2;
- int pfd[2],pid;
- long sdel,nsdel;
- char msg[12];
+// struct timespec t1,t2;
+ int pid; // pfd[2]
+// long sdel,nsdel;
+// char msg[12];
 
+ char s[150];
+ memset(s,0,150);
 // pipe(pfd);
  pid=fork();
  if(pid==0)
   {
-  close(3);
+     pid=getpid();
+     // sprintf(s,"cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq | tr  '\n' '|' | tee -a tmp\n"
+     //           "ps --no-headers -o psr -p %i | tee -a tmp",getpid());
+     sprintf(s,"cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq | tr  '\n' '|' >> tmp\n"
+               "ps --no-headers -o psr -p %i >> tmp",getpid());
+     //  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&t1);
+     //... какая-нибудь нагрузка
+     for(unsigned i=0;i<lcnt;++i)
+      {
+      //  printf("%i\n",i);
+       system(s);
+      }
+      //  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&t2);
+      //  sdel=t2.tv_sec-t1.tv_sec;
+      //  if(t2.tv_nsec-t1.tv_nsec<0)
+      //   nsdel=1000000000+t2.tv_nsec-t1.tv_nsec;
+      //  else
+      //   nsdel=t2.tv_nsec-t1.tv_nsec;
 
-//  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&t1);
-  //... какая-нибудь нагрузка
-  for(unsigned i=0;i<=lcnt;)
-   i++;
-//  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&t2);
-//  sdel=t2.tv_sec-t1.tv_sec;
-//  if(t2.tv_nsec-t1.tv_nsec<0)
-//   nsdel=1000000000+t2.tv_nsec-t1.tv_nsec;
-//  else
-//   nsdel=t2.tv_nsec-t1.tv_nsec;
-
-//  close(1);
-//  dup(4);
-//  printf("%1li.%.9li",sdel,nsdel);
+      //  close(1);
+      //  dup(4);
+      //  printf("%1li.%.9li",sdel,nsdel);
   exit(1);
   }
 // close(4);
@@ -149,25 +158,39 @@ double get_rand(const unsigned lcnt)
 // read(3,msg,11);
 // msg[11]=0;
 // close(3);
+ unsigned freq_cpu[4],cpu_core;
+ FILE *ftmp;
+
+ ftmp=fopen("tmp","r");
+ *freq=0;
+ for(unsigned i=0;i<lcnt;++i)
+  {
+  fscanf(ftmp,"%i|%i|%i|%i|  %i",&freq_cpu[0],&freq_cpu[1],&freq_cpu[2],&freq_cpu[3],&cpu_core);
+  *freq+=freq_cpu[cpu_core];
+  }
+ system("> tmp");
+ close(4);
  return info.ru_stime.tv_sec+info.ru_utime.tv_sec+(info.ru_stime.tv_usec+info.ru_utime.tv_usec)*0.000001;
  }
 
 double get_rand_devi_sum(const double meanX, const unsigned lcnt, const unsigned n)
  {
  double sum=0.;
+ unsigned freq;
  for(unsigned i=0;i<n;++i)
-  sum+=get_rand_devi(meanX,lcnt);
+  sum+=get_rand_devi(meanX,lcnt,&freq);
  return sum;
  }
 
-int distribution_init(double *meanX, double *meanS, double *varS, const unsigned cnt,
+void distribution_init(double *meanX, double *meanS, double *varS, const unsigned cnt,
                       const unsigned lcnt, const unsigned n)
  {
- double sample[cnt],_meanX=0.,_meanS=0.,_varS;
+ double sample[cnt],_meanX=0.,_meanS=0.,_varS=0.;
+ unsigned freq; //временно так
 
  for(unsigned k=0;k<cnt;++k)
   {
-  sample[k]=get_rand(lcnt);
+  sample[k]=get_rand(lcnt,&freq);
   _meanX+=sample[k];
   }
  _meanX/=cnt;
@@ -201,13 +224,15 @@ int distribution_init(double *meanX, double *meanS, double *varS, const unsigned
  *varS=_varS;
  }
 
-int distribution_init_test(double *meanX, double *meanS, double *varS, const unsigned cnt,
-                      const unsigned lcnt, const unsigned n)
+void distribution_init_test(double *meanX, double *meanS, double *varS, const unsigned cnt,
+                      const unsigned lcnt, const unsigned n, char *fname)
  {
  double _meanX=0,_meanS=0,tmp=0;
- double _varS;
+ double _varS=0.;
  const unsigned m=cnt/n;
  double S[m],X[cnt],rnd;
+ unsigned freq[cnt];
+ system("> tmp");
  for(unsigned i=0;i<m;++i)
   S[i]=0;
  for(unsigned i=0;i<m;++i)
@@ -215,7 +240,7 @@ int distribution_init_test(double *meanX, double *meanS, double *varS, const uns
   for(unsigned j=0;j<n;++j)
    {
 //   kek:
-   rnd=get_rand(lcnt);
+   rnd=get_rand(lcnt,&freq[i*n+j]);
    //МОЙ ФИЛЬТР
    //Квантиль 0.999 - 13950; 0.99 - 1735
 //   if(rnd==0||rnd>13950)
@@ -224,7 +249,7 @@ int distribution_init_test(double *meanX, double *meanS, double *varS, const uns
    S[i]+=rnd;
    }
   tmp+=S[i];
-  fprintf(stderr,"\r%i/%i",i,m);
+  printf("\r%i/%i",i,m);
   }
 
  _meanX=tmp/cnt;
@@ -241,19 +266,29 @@ int distribution_init_test(double *meanX, double *meanS, double *varS, const uns
  _varS/=m;
 
 //Вывод сумм S
+ creat(fname,0664);
+ FILE *ffile=fopen(fname,"w");
  for(unsigned i=0;i<m;++i)
   {
-  printf("%f\n",S[i]); //normal_cdf(S[i],_meanS,_varS) - как аргумент
-  fflush(stdout);
+  fprintf(ffile,"%f\n",S[i]); //normal_cdf(S[i],_meanS,_varS) - как аргумент
+  fflush(ffile);
   }
 
 //Вывод значений X
- close(1);
  creat("X",0664);
+ FILE* fXfile=fopen("X","w");
  for(unsigned i=0;i<cnt;++i)
   {
-  printf("%f\n",X[i]);
-  fflush(stdout);
+  fprintf(fXfile,"%f\n",X[i]);
+  fflush(fXfile);
+  }
+//Вывод значений Xfreq
+ creat("Xfreq",0664);
+ FILE* fXfreqfile=fopen("Xfreq","w");
+ for(unsigned i=0;i<cnt;++i)
+  {
+  fprintf(fXfreqfile,"%i\n",freq[i]);
+  fflush(fXfreqfile);
   }
 
  *meanX=_meanX;
