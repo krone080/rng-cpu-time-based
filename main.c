4e7d89318af7978c69a2ba7ae5c3d7866debe283
Ivan Pavlov (u2) <stone08@mail.ru>
2019-06-20 08:07:04 +0300

    C15: просто пара прототипов и другая мелочь

--- main.c
+++ main.c
@@ -12,8 +12,6 @@
 #include <math.h>
 #include <string.h>
 
-//Получение параметров нормального распределения в текущий момент
-//времени
 double get_rand(const unsigned lcnt, unsigned *freq);
 
 double get_rand_devi(const double meanX, const unsigned lcnt,unsigned *freq)
@@ -27,30 +25,22 @@ void distribution_init(double *meanX, double *meanS, double *varS, const unsigne
 void distribution_init_test(double *meanX, double *meanS, double *varS, const unsigned cnt,
                       const unsigned lcnt, const unsigned n, char *fname);
 
+void freq_splitting_mode();
+
 double normal_cdf(double x, double E, double D)
  {return 0.5*(1.+erf((x-E)/sqrt(2.*D)));}
 
+void get_bit_sequence(double num, char* seq, unsigned split_len);
+
 pid_t pfd_to[2],pfd_from[2];
-/*
- *
- *
- *
- *
- *
- *
- *
- */
+
 int main(int argc,char* argv[])
  {
-//1. трансформация процесса в демона
-
-//2. fork, создание особых потомков для генерации различных
-//   независимых выборок
 
  pipe(pfd_to);
  pipe(pfd_from);
 
-//Процесс-посредник
+//Процесс <Посредник>
  if(fork()==0)
   {
   close(pfd_to[1]);
@@ -64,17 +54,13 @@ int main(int argc,char* argv[])
    char s[150];
    memset(s,0,150);
 
-//Генерируемый процесс для выработки
+//Генерируемый процесс <Задание>
    if(fork()==0)
     {
-    // sprintf(s,"cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq | tr  '\n' '|' | tee -a tmp\n"
-    //           "ps --no-headers -o psr -p %i | tee -a tmp",getpid());
     sprintf(s,"cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq | tr  '\n' '|' >> tmp\n"
-                "ps --no-headers -o psr -p %i >> tmp",getpid());
-    //... какая-нибудь нагрузка
+              "ps --no-headers -o psr -p %i >> tmp",getpid());
     for(unsigned i=0;i<lcnt;++i)
      {
-     //  printf("%i\n",i);
      system(s);
      }
     exit(1);
@@ -84,15 +70,12 @@ int main(int argc,char* argv[])
    }
   exit(1);
   }
-
  close(pfd_to[0]);
  close(pfd_from[1]);
 
  char fname[20];
  unsigned cnt,n,cnt2,lcnt;
  FILE *ffile;
-// float rtime,ptime,ipc;
-// long long ins;
 
  if(argc==6)
   {
@@ -116,44 +99,14 @@ int main(int argc,char* argv[])
 
   scanf("%i",&cnt2);
   }
-// fd=creat(fname,0664);
-// if(fd<0)
-//  {
-//  perror("creat()");
-//  return -1;
-//  }
-
-
-// ffile=fopen(fname,"w");
-// Параметры цикла (счётчик) будут, скорее всего, изменяемыми и будут
-// зависеть от оценок, посчитанных заранее
 
  double varS;
  double meanX,meanS;
  distribution_init_test(&meanX,&meanS,&varS,cnt,lcnt,n,fname);
 
-// printf("\nES=%lli, DS=%e",meanS,varS);
-// for(unsigned i=0;i<cnt2;++i)
-//  {
-//  printf("%.9f ",get_rand_devi_sum(meanX,lcnt,n));
-//  fflush(stdout);
-//  }
-//4. преобразование
-
-//5. предоставление числа/последовательности на выход
-
  return 0;
  }
-/*
- *
- *
- *
- *
- *
- *
- *
- *
- */
+
 double get_rand(const unsigned lcnt, unsigned *freq)
  {
  struct rusage info;
@@ -173,7 +126,6 @@ double get_rand(const unsigned lcnt, unsigned *freq)
   }
  close(3);
  system("> tmp");
-// close(4);
  return info.ru_stime.tv_sec+info.ru_utime.tv_sec+(info.ru_stime.tv_usec+info.ru_utime.tv_usec)*0.000001;
  }
 
@@ -243,12 +195,7 @@ void distribution_init_test(double *meanX, double *meanS, double *varS, const un
   {
   for(unsigned j=0;j<n;++j)
    {
-//   kek:
    rnd=get_rand(lcnt,&freq[i*n+j]);
-   //МОЙ ФИЛЬТР
-   //Квантиль 0.999 - 13950; 0.99 - 1735
-//   if(rnd==0||rnd>13950)
-//    goto kek;
    X[i*n+j]=rnd;
    S[i]+=rnd;
    }
@@ -262,7 +209,7 @@ void distribution_init_test(double *meanX, double *meanS, double *varS, const un
  for(unsigned k=0;k<m;++k)
   S[k]-=_meanX*n;
 
- //Небольшая погрешность(хотя кто знает) в обмен на производительность
+//Небольшая погрешность
  _meanS=tmp/m-_meanX*n;
 
 //вычисление дисперсии
@@ -299,5 +246,25 @@ void distribution_init_test(double *meanX, double *meanS, double *varS, const un
  *meanX=_meanX;
  *meanS=_meanS;
  *varS=_varS;
+ }
 
+void get_bit_sequence(double num, char* seq, unsigned split_len)
+ {
+ memset(seq,0,split_len+1);
+ unsigned shift=2;
+ double position=0.5;
+ for(unsigned i=0;i<split_len;i++)
+  {
+  if(num<position)
+   {
+   seq[i]='0';
+   position-=1/shift;
+   }
+  else
+   {
+   seq[i]='1';
+   position+=1/shift;
+   }
+  shift<<=1;
+  }
  }
